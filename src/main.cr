462b29244b70542a6178cad939051524bf619d16
itang <live.tang@gmail.com>
2015-06-16 18:59:42 +0800

    update

--- src/main.cr
+++ src/main.cr
@@ -8,7 +8,8 @@ include Haoshuju
 
 WD = Dir.working_directory
 PORT = ENV.has_key?("PORT") ? ENV["PORT"].to_i : 3000
-puts "work dir: #{WD}, port: #{PORT}"
+APPPATH = File.expand_path($0)
+puts "$0: #{$0}, app_path: #{APPPATH}, work dir: #{WD}, port: #{PORT}"
 
-app = HaoshujuApp.new(WD)
+app = HaoshujuApp.new(APPPATH)
 app.serve PORT
