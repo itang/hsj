634d10a704eb72e42994af1df6fa438412116ff5
itang <live.tang@gmail.com>
2015-07-08 12:08:59 +0800

    more

--- src/haoshuju/models/read_service.cr
+++ src/haoshuju/models/read_service.cr
@@ -15,8 +15,12 @@ end
 
 class ReadService
   def find_reads
-    [Read.new "http://www.slideshare.net/rolandkuhn/akka-typed-between-session-types-and-the-actor-model",
+    read2 = Read.new "http://www.slideshare.net/rolandkuhn/akka-typed-between-session-types-and-the-actor-model",
               title: "Akka Typed — between Session Types and the Actor Model",
-              created_at: "2015-07-07 22:35"]
+              created_at: "2015-07-07 22:35"
+    read1 = Read.new "https://scotch.io/tutorials/getting-started-with-vim-an-interactive-guide",
+              title: "Getting Started with Vim: An Interactive Guide",
+              created_at: "2015-07-08 12:06"
+    [read1, read2]
   end
 end
