db270128419acdd071d6f3d243228a78efff75b8
itang <live.tang@gmail.com>
2015-08-24 15:20:33 +0800

    cleanup

--- src/haoshuju/controllers/welcome_controller.cr
+++ src/haoshuju/controllers/welcome_controller.cr
@@ -1,8 +1,6 @@
 class WelcomeController < BaseController
   actions :index
 
-  @@injector = Haoshuju::Injector.instance
-
   @weibos = [] of Weibo
   @keywords = [] of Keyword
   @languages = Array(Language).new
@@ -12,12 +10,12 @@ class WelcomeController < BaseController
   view "index", "#{__DIR__}/../views/welcome"
   def index
     @name = "Itang"
-    @weibos = @@injector.weibo_service.find_weibos
-    @keywords = @@injector.keyword_service.find_keywords
-    @languages = @@injector.language_service.find_languages
-    @reads = @@injector.read_service.find_reads
+    @weibos = injector.weibo_service.find_weibos
+    @keywords = injector.keyword_service.find_keywords
+    @languages = injector.language_service.find_languages
+    @reads = injector.read_service.find_reads
+    @dicts = injector.dict_service.find_all(Pager.new)
     @build_time = get_build_time
-    @dicts = @@injector.dict_service.find_all(Pager.new())
 
     respond_to do |format|
       format.html { render "index" }
