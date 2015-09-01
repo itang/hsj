362039fbe48b2dfdb6b37144f2013aba9f707bf4
itang <live.tang@gmail.com>
2015-09-01 21:26:18 +0800

    cleanup

--- src/haoshuju/middlewares/api_auth.cr
+++ src/haoshuju/middlewares/api_auth.cr
@@ -1,12 +1,15 @@
 require "amethyst"
 
 class ApiAuthMiddleware < Middleware::Base
+  AUTH_HEADER_KEY = "AUTH"
+  TOKEN_SEPARATOR = ";"
+
   # TODO load from config(db) or ENV
   @auths = {"test" => {name: "test", client_id: "test", token: "test2015"}}
 
   def call(request : Http::Request)
     if request.path.starts_with? "/api"
-      unless auth?(request.headers["AUTH"]?)
+      unless auth?(request.headers[AUTH_HEADER_KEY]?)
         puts "auth not passed!"
         return Response.new(401, "未授权").tap &.header("Content-Type", "text/plain; charset=utf-8")
       else
@@ -19,7 +22,7 @@ class ApiAuthMiddleware < Middleware::Base
 
   private def auth?(auth_str: String?): Bool
     if auth_str
-      auth = auth_str.split(";")
+      auth = auth_str.split(TOKEN_SEPARATOR)
       if auth.length == 2
         client_id, token = auth
         a = @auths[client_id]?
