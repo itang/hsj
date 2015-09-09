5e84095f7f803044f45ecf81994449cb99f9e3d5
itang <live.tang@gmail.com>
2015-09-09 21:58:03 +0800

    more reads

--- src/haoshuju/models/read_service.cr
+++ src/haoshuju/models/read_service.cr
@@ -120,6 +120,10 @@ module Haoshuju
           rs << Read.new "http://phoenix.thefirehoseproject.com/0.html",
                     title: "Phoenix tutorial",
                     created_at: ""
+
+          rs << Read.new "http://openresty.org/download/agentzh-nginx-tutorials-zhcn.html",
+                    title: "agentzh 的 Nginx 教程",
+                    created_at: "2015-09-09 21:57",
         end.reverse
       end
     end
