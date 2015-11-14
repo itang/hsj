01bdc7af40514abe1fed8d87b237298db61ac762
itang <live.tang@gmail.com>
2015-11-14 17:04:01 +0800

    more

--- src/haoshuju/models/language_service.cr
+++ src/haoshuju/models/language_service.cr
@@ -9,8 +9,8 @@ class LanguageService
   def find_languages
     [Language.new("Scala", url = "http://www.scala-lang.org"),
       Language.new("Ruby", url = "http://www.ruby-lang.org"),
-      Language.new("Elixir", url = "http://www.elixir-lang.org"),
       Language.new("Scala.js", url = "http://scala-js.org/"),
+      Language.new("Elixir", url = "http://www.elixir-lang.org"),
       Language.new("Rust", url = "http://www.rust-lang.org"),
       Language.new("Crystal", url = "http://www.crystal-lang.org")]
   end
