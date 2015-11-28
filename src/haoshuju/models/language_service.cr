5b610099a9ed1c95a66071df50fa2bb04d1043b7
itang <live.tang@gmail.com>
2015-11-28 21:23:30 +0800

    more

--- src/haoshuju/models/language_service.cr
+++ src/haoshuju/models/language_service.cr
@@ -1,17 +1,18 @@
 class Language
-  property name, url, hots
+  property name, url, level, hots
 
-  def initialize(@name : String, @url = "" : String, @hots = 100 : Int32)
+  def initialize(@name : String, @url = [] of String, @level = 1 : Int32, @hots = 100 : Int32)
   end
 end
 
 class LanguageService
   def find_languages
-    [Language.new("Scala", url = "http://www.scala-lang.org"),
-      Language.new("Ruby", url = "http://www.ruby-lang.org"),
-      Language.new("Scala.js", url = "http://scala-js.org/"),
-      # Language.new("Elixir", url = "http://www.elixir-lang.org"),
-      Language.new("Rust", url = "http://www.rust-lang.org"),
-      Language.new("Crystal", url = "http://www.crystal-lang.org")]
+    [Language.new("Scala(.js)", url = ["http://www.scala-lang.org", "http://scala-js.org/"]),
+      Language.new("(J)Ruby", url = ["http://www.ruby-lang.org", "http://www.jruby.org"]),
+      Language.new("Elixir", url = ["http://www.elixir-lang.org"], level = 2),
+      Language.new("Rust", url = ["http://www.rust-lang.org"]),
+      Language.new("Crystal", url = ["http://www.crystal-lang.org"], level = 2),
+      Language.new("Go", url = ["http://www.golang.org"], level = -1),
+      Language.new("Clojure", url = ["http://www.clojure.org"], level = -1)]
   end
 end
