6b7ee05442e859f3b326af6f3ce4db575e57e076
itang <live.tang@gmail.com>
2016-01-22 23:23:12 +0800

    more

--- src/haoshuju/models/language_service.cr
+++ src/haoshuju/models/language_service.cr
@@ -9,11 +9,11 @@ class LanguageService
   def find_languages
     [Language.new("Scala", url = ["http://www.scala-lang.org", "http://scala-js.org/"]),
       Language.new("Ruby", url = ["http://www.ruby-lang.org", "http://www.jruby.org"]),
-      Language.new("Rust", url = ["http://www.rust-lang.org"]),
+      Language.new("Rust", url = ["http://www.rust-lang.org"], level = 2),
       Language.new("Kotlin", url = ["http://kotlinlang.org"], 2),
       Language.new("TypeScript", url = ["http://www.typescriptlang.org/"], 2),
-      Language.new("Swift", url = ["http://swift.org"], 2),
-      Language.new("Crystal", url = ["http://www.crystal-lang.org"], level = 2),
+      Language.new("Swift", url = ["http://swift.org"], 4),
+      Language.new("Crystal", url = ["http://www.crystal-lang.org"], level = 4),
       Language.new("Elixir", url = ["http://www.elixir-lang.org"], level = -2),
       Language.new("Go", url = ["http://www.golang.org"], level = 3),
       Language.new("Clojure", url = ["http://www.clojure.org"], level = -1)]
